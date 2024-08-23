ecea2ceaf186611e7b70528d8e5d54e21766ac78
MaiberCordoba <cordobamaiber@gmail.com>
2024-08-22 22:57:09 -0500

    creando en la app users  la tabla perosona

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    ESTADO_CHOICE = [('Activo', 'Activo'), ('Inactivo', 'Inactivo')]
+    nombre = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    fecha_Creacion = models.DateField(auto_now=True)
+    estado = models.CharField(max_length=50, choices=ESTADO_CHOICE, default="Activo") 
+    def __str__(self):
+        return self.nombre 
